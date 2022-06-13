d1cbcde5311bfbd6fbe9578817f66c5d902e903a
Hunter Nuga <94147854+hunternuga@users.noreply.github.com>
2022-06-12 19:43:21 -0600

    Create fun-calculator-thing.py

---
+++ fun-calculator-thing.py
@@ -0,0 +1,5 @@
+first_number = input("Enter a number: ")
+second_number = input("Enter a second number: ")
+
+sum = int(first_number) + int(second_number)
+print("The sum of the two numbers is: " + str(sum))
